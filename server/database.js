0597d30c6466bd7e753dd5014bd85efaa9b827b4
lucasareguati <33659055+lucasareguati@users.noreply.github.com>
2019-02-06 10:39:47 -0300

    Update database.js

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ var Sequelize = require('sequelize');
 
 //Conexión de sequelize con Postgres
 
-const sequelize = new Sequelize('postgres://postgres:39842189@localhost:5432/gustekadb');
+const sequelize = new Sequelize('postgres://usuariopostgres:contraseña@localhost:5432/nombreBBDD
 
 //Autenticacion 
 sequelize.authenticate().then(()=>{
